cc7dc1f8564cdaaff66fa35cf6a07f2aa9c7ba64
unknown <stewartjumbe@gmail.com>
2023-12-27 14:48:48 +0000

    successfully serving static index.html when server is running

--- index.js
+++ index.js
@@ -3,23 +3,51 @@
 //importing hapi framework
 const Hapi = require('@hapi/hapi');
 
+//importing path
+const Path = require ('path')
+
 //Creating a server 
-const init = async () => {
+const start = async () => {
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        routes:{
+            files:{
+                relativeTo:
+                Path.join(__dirname,'public')
+            }
+        }
     });
+
+    // registering hapi/inert plugin
+await server.register(require('@hapi/inert'));
+
+
+//serving index.html file
+server.route({
+    method: 'GET',
+    path: '/index',
+    handler: (request, h) => {
+        
+        return h.file('index.html');
+    
+    }   
+});
 
 
 server.route({
     method: 'GET',
-    path: '/',
+    path: '/hello',
     handler: (request, h) => {
         return ' Hello World';
+    },
+    options: {
+        files:null // overides the server option for this route
     }
 });
 
     await server.start();
+    console.log(Path.join(__dirname,'public'))
     console.log('Server is running on %s', server.info.uri);
 };
 
@@ -28,5 +56,5 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
 });
 
-init();
+start();
 
