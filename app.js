08f52a5159dfcc7c0dd5425b0fe753283219135f
unknown <stewartjumbe@gmail.com>
2024-01-15 13:54:26 +0000

    added standardjs for linting

--- app.js
+++ app.js
@@ -1,106 +1,106 @@
 // index.js
-'use strict';
+'use strict'
 
 // Importing hapi framework
-const Hapi = require('@hapi/hapi');
+const Hapi = require('@hapi/hapi')
 
 // Importing path
-const Path = require ('path')
+const Path = require('path')
 
 // Importing fileHandler class able to handle vides and photos
-const FileHandler = require('./fileHandler');
+const FileHandler = require('./fileHandler')
 
 // Instantiating imageHandler
-const fileHandler = new FileHandler(Path.join(__dirname, 'files'));
+const fileHandler = new FileHandler(Path.join(__dirname, 'files'))
 
-// Creating a server 
+// Creating a server
 const start = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0',//'0.0.0.0', // Changed from 'localhost to make the sever accessible on local network
-        routes:{
-            files:{
-                relativeTo: Path.join(__dirname,'public')
-            }
+  const server = Hapi.server({
+    port: 3000,
+    host: '0.0.0.0', // '0.0.0.0', // Changed from 'localhost to make the sever accessible on local network
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public')
+      }
+    }
+  })
+
+  // Registering hapi/inert plugin
+  await server.register(require('@hapi/inert'))
+
+  // GET req serving index.html file
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return h.file('index.html')
+    }
+  })
+
+  // POST request saving uploaded pictures
+  server.route({
+    method: 'POST',
+    path: '/upload',
+    options: {
+      payload: {
+        output: 'stream',
+        parse: true,
+        allow: 'multipart/form-data',
+        multipart: true,
+        maxBytes: 2147483648 // Increase max payload size to 2GB
+
+      }
+    },
+    handler: async (request, h) => {
+      const data = request.payload
+      const name = data.name
+      const comment = data.comment
+      const file = data.file
+
+      // Log relevant information
+      console.log(`Name: ${name}`)
+      console.log(`Comment: ${comment}`)
+      console.log('File:', file)
+
+      // Check if file data is present
+      if (file) {
+        console.log('filename: ', file.hapi.filename)
+
+        // Ensure that file data is directly accessible
+        // collecting chuncks of data into buffer
+        const chunks = []
+        for await (const chunk of file) {
+          chunks.push(chunk)
         }
-    });
+        const buffer = Buffer.concat(chunks)
+        await fileHandler.saveFile(file.hapi.filename, buffer)
+        return h.response('Image uploaded successfully!')
+      } else {
+        return h.response('No image data received. Please ensure you are uploading a valid image file.').code(400)
+      }
+    }
+  })
 
-    // Registering hapi/inert plugin
-    await server.register(require('@hapi/inert'));
+  // GET req serving "Hello World"
+  server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: (request, h) => {
+      return ' Hello World'
+    },
+    options: {
+      files: null // Overrides the server option for this route
+    }
+  })
 
-    // GET req serving index.html file
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return h.file('index.html');
-        }   
-    });
-
-    // POST request saving uploaded pictures
-    server.route({
-        method:'POST',
-        path: '/upload',
-        options: {
-            payload:{
-                output: 'stream',
-                parse: true,
-                allow: 'multipart/form-data',
-                multipart:true,
-                maxBytes: 2147483648 // Increase max payload size to 2GB
-
-            }
-        },
-        handler: async (request, h) => {
-            const data = request.payload;
-            const name = data.name;
-            const comment = data.comment;
-            const file = data.file;
-
-            // Log relevant information
-            console.log(`Name: ${name}`);
-            console.log(`Comment: ${comment}`);
-            console.log('File:', file);
-
-            // Check if file data is present
-            if (file) {
-                console.log('filename: ', file.hapi.filename);
-
-                // Ensure that file data is directly accessible
-                //collecting chuncks of data into buffer
-                let chunks = [];
-                for await (let chunk of file) {
-                    chunks.push(chunk);
-                }
-                let buffer = Buffer.concat(chunks);
-                await fileHandler.saveFile(file.hapi.filename, buffer);
-                return h.response('Image uploaded successfully!');
-            } else {
-                return h.response('No image data received. Please ensure you are uploading a valid image file.').code(400);
-            }
-        }
-    });
-
-    // GET req serving "Hello World"
-    server.route({
-        method: 'GET',
-        path: '/hello',
-        handler: (request, h) => {
-            return ' Hello World';
-        },
-        options: {
-            files:null // Overrides the server option for this route
-        }
-    });
-
-    await server.start();
-    console.log(Path.join(__dirname,'public'))
-    console.log('Server is running on %s', server.info.uri);
-};
+  await server.start()
+  console.log(Path.join(__dirname, 'public'))
+  console.log('Server is running on %s', server.info.uri)
+}
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-});
+  console.log(err)
+  process.exit(1)
+})
 
-start();
+start()
--- media_website_db.js
+++ media_website_db.js
@@ -1,26 +1,19 @@
-'use strict';
+'use strict'
 
-
-const { Client } = require('pg');
-
+const { Client } = require('pg')
 
 const client = new Client({
-    host:'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'admin',
-    database: 'media_website_db'
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'admin',
+  database: 'media_website_db'
 
+})
 
-});
+async function insertData () {
+  await client.connect()
 
-async function insertData() {
-    await client.connect();
-
-    const text = 'INSERT INTO files(file_name, comment, username, save_directory) VALUES( )';
-    const values = ['file_']
-
-
+  const text = 'INSERT INTO files(file_name, comment, username, save_directory) VALUES( )'
+  const values = ['file_']
 }
-
-
